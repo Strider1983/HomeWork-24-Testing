74ef711bda7b136d5b35cbb1365a764899d946ef
mezen <mezentsev.k@gmail.com>
2023-12-17 19:57:17 +0800

    Start

---
+++ src/main/java/ru/skypro/testcalculator/TestcalculatorApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.testcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/testcalculator/TestcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.testcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
